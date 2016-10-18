15132cca9a8cd057aeb63e82a3b3eb2bdaeea491
diasroyal <diasaroyal@gmail.com>
2016-10-18 11:12:43 -0400

    Update response.php

--- response.php
+++ response.php
@@ -1,6 +1,6 @@
 <?php
     header("content-type: text/xml");
-	$icecream = array(array("Which ice-cream would you like to have today ?",array("1. Banana Nut Fudge", "2. Black Walnut", "3. Burgundy Cherry", "4. Butterscotch Ribbon","5. Cherry Macaroon","6. Chocolate","7. Chocolate Almond","8. Chocolate Chip","9. Chocolate Fudge","10. Chocolate Mint","11. Chocolate Ribbon","12. Coffee","13. Coffee Candy","14. Date Nut","15. Egg Nog","16. French Vanilla","17. Green Mint Stick","18. Lemon Crisp","19. Lemon Custard","20. Lemon Sherbet","21. Maple Nut","22. Orange Sherbet", "23. Peach","24. Peppermint Fudge Ribbon","25. Peppermint Stick","26. Pineapple Sherbet","27. Raspberry Sherbet","28. Rocky Road","29. Strawberry","30. Vanilla","31. Vanilla Burnt Almond")));
+	$icecream = array("0"=>"Please select your flavour: ", "1"=>" Banana Nut Fudge", "2"=>" Black Walnut", "3"=>" Burgundy Cherry", "4"=>" Butterscotch Ribbon","5"=>" Cherry Macaroon","6"=>" Chocolate","7"=>" Chocolate Almond","8"=>" Chocolate Chip","9"=>" Chocolate Fudge","10"=>" Chocolate Mint","11"=>" Chocolate Ribbon","12"=>" Coffee","13"=>" Coffee Candy","14"=>" Date Nut","15"=>" Egg Nog","16"=>" French Vanilla","17"=>" Green Mint Stick","18"=>" Lemon Crisp","19"=>" Lemon Custard","20"=>" Lemon Sherbet","21"=>" Maple Nut","22"=>" Orange Sherbet", "23"=>" Peach","24"=>" Peppermint Fudge Ribbon","25"=>" Peppermint Stick","26"=>" Pineapple Sherbet","27"=>" Raspberry Sherbet","28"=>" Rocky Road","29"=>" Strawberry","30"=>" Vanilla","31"=>" Vanilla Burnt Almond")));
 	$icecreamanswer   = 0;
     $quiz = array(
 	    "icecream" => $icecream,		
@@ -9,12 +9,9 @@
 	// $from   = $_REQUEST['from'];
 	$answer = $_REQUEST['Body'];
 	$reply  = array();
-	if (is_numeric($answer)) {
-		if($answer == 2){
-			$reply = 'Correct answer'.$quiz[$answer][0][1].'On';
-		}
-		else{
-			$reply = 'Wrong answer';
+		if (is_numeric($answer)) {
+		
+			$reply ="Your order for ".$icecream[1][answer]."is placed.";
 		}
 	}
 	else if(is_string($answer)){
@@ -24,6 +21,7 @@
 			array_push($reply, $value);
 		}
 	}
+
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 
