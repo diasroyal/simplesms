cb528c102dda597d90f7081b670cc6da09cf60ec
diasroyal <diasaroyal@gmail.com>
2016-10-19 05:12:00 -0400

    Update response.php

--- response.php
+++ response.php
@@ -1,45 +1,69 @@
 <?php
 	session_start();
-
     header("content-type: text/xml");
-
-	$icecream = array("Select IceCream:"," Banana Nut Fudge"," Black Walnut"," Burgundy Cherry"," Butterscotch Ribbon"," Cherry Macaroon"," Chocolate"," Chocolate Almond"," Chocolate Chip"," Chocolate Fudge"," Chocolate Mint"," Chocolate Ribbon"," Coffee"," Coffee Candy"," Date Nut"," Egg Nog"," French Vanilla"," Green Mint Stick"," Lemon Crisp"," Lemon Custard"," Lemon Sherbet"," Maple Nut"," Orange Sherbet"," Peach"," Peppermint Fudge Ribbon"," Peppermint Stick"," Pineapple Sherbet"," Raspberry Sherbet"," Rocky Road"," Strawberry"," Vanilla"," Vanilla Burnt Almond");
- $icecreamanswer   = 0;
-    $quiz = array(
-	    "icecream" => $icecream,		
-	);
-	// $to 	= $_REQUEST['to'];
-	// $from   = $_REQUEST['from'];
-	$answer = $_GET['Body'];
-	foreach($icecream as $x_value) {
-        echo "Value=" . $x_value;
-        echo "<br>";
+	$answer = $_POST['Body'];
+	if (strpos($answer, 'start') !== FALSE || strpos($answer, 'restart') !== FALSE) {
+		$_SESSION['answer']='';
+		$reply['welcome'] = printqt();
 	}
-	if (is_numeric($answer)) {
-			$icecream = $icecream[$answer]; 
-			$reply ="Your order for".$icecream."is placed.";
+	elseif (strpos($answer, 'add') !== FALSE || strpos($answer, 'sub') !== FALSE || strpos($answer, 'mul') !== FALSE || strpos($answer, 'div') !== FALSE) {
+		$x = rand(20,30);
+		$y = rand(1,10);
+		switch ($answer) {
+			case 'add':
+				$reply = $x.' + '.$y;
+				$_SESSION['answer']=$x+$y;
+				break;
+			case 'sub':
+				$reply = $x.' - '.$y;
+				$_SESSION['answer']=$x-$y;
+				break;
+			case 'mul':
+				$reply = $x.' * '.$y;
+				$_SESSION['answer']=$x*$y;
+				break;
+			case 'div':
+				$reply = $x.' / '.$y;
+				$_SESSION['answer']=$x/$y;
+				break;	
+			default:
+				$makereply = 'Enter valid numer or type "restart" to start again'.PHP_EOL;
+				break;
+		}
 	}
-else if(is_string($answer)){
-		array_merge($icecream,$reply);
-				}
+	elseif (is_numeric($answer) && is_numeric($_SESSION['answer'])) {
+		if($answer == $_SESSION['answer']){
+			$reply['prev'] = 'Correct answer'.PHP_EOL.PHP_EOL;
+			$reply['welcome'] = printqt();
+		}
+		else{
+			$reply['prev'] = 'Wrong answer'.PHP_EOL.PHP_EOL;
+			$reply['welcome'] = printqt();
+		}
 	}
-//	$replyx=array();
-//		else if(is_string($answer)){
-//			$replyx=$icecream;
-//		}
-       
+	else{
+		$_SESSION['answer']='';
+		$reply['welcome'] = printqt();
+	}
+	// print_r($_SESSION);
+	function printqt(){
+		$makereply = 'Choose one of the following :'.PHP_EOL.PHP_EOL.'add for ADDITION'.PHP_EOL.'sub for SUBTRACTION'.PHP_EOL.'mul for MULTIPLICATION'.PHP_EOL.'div for DIVIVSION'.PHP_EOL;
+		return $makereply;
+	}
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 
 <Response>
 	<Sms>
 			<?php
-				
-					foreach($reply as $value){
+				if(is_array($reply)){
+					foreach($reply as $key => $value){
 						echo $value;
 					}
-				
-				
+				}
+				else{
+					echo $reply;
+				}
 			?>
 	</Sms>
 </Response>
